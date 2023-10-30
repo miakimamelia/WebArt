464ea43b2c3feed050b61dbe1cf662a640d9416b
miakimamelia <128097163+miakimamelia@users.noreply.github.com>
2023-10-30 15:36:12 -0500

    day 16 added

---
+++ day16/javascript_demo/main.js
@@ -0,0 +1,35 @@
+console.log("Hello World")
+
+let a = 2;
+let b = 3;
+
+let c = a + b;
+
+let myName = "amelia"
+
+console.log(c);
+console.log(myName)
+
+for(let i=0; i < 100; i++) {
+    console,localStorage(i);
+}
+
+// store a reference to myButton in Javascript
+let myButton = document.getElementById("myButton")
+
+// add an event listener that listens for when the user clicks on the button 
+
+myButton.addEventListener("click", function () {
+
+    //the function body, the stuff that should happen when the button is clicked
+
+    console.log("button was clicked")
+
+    //store a reference to the circle
+    let myCircle = document.getElementById
+    ("myCircle")
+
+    //change the background color of the circle
+    myCircle.style.backgroundColor = "orangered"
+
+});
